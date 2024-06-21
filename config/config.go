ddbc882bf6c3789e36d999fb0fd79050f43a31fd
sudo-odner <odner.main@gmail.com>
2024-06-21 18:11:56 +0300

    add config and todo in app file

--- config/config.go
+++ config/config.go
@@ -1,1 +1,45 @@
 package config
+
+import (
+	"errors"
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Api_key                  string `yaml:"API_KEY"`
+	Url_get_fiat             string `yaml:"url_get_fiat"`
+	Url_get_cryptocurrencies string `yaml:"url_get_cryptocurrencies"`
+	Url_convert_one_to_one   string `yaml:"url_convert_one_to_one"`
+}
+
+func Load() (*Config, error) {
+
+	// Провекрка существования .env
+	if err := godotenv.Load(); err != nil {
+		log.Print(".env file found")
+		return nil, err
+	}
+
+	configPath, exists := os.LookupEnv("CONFIG_PATH")
+	if !exists {
+		log.Fatal("CONFIG_PATH is not found")
+		return nil, errors.New("CONFIG_PATH is not found")
+	}
+
+	// Провека на существования файла
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+		return nil, err
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+		return nil, err
+	}
+
+	return &cfg, nil
+}
---
+++ internal/app/app.go
@@ -0,0 +1,14 @@
+package app
+
+import "github.com/sudo-odner/CurrencyConverter/internal/controler/protHttp"
+
+func app() {
+	// TODO init config
+	// TODO init model
+	// TODO init usecase
+	// TODO init client
+	client := protHttp.New()
+	// TODO init db
+	// TODO save some date to from api to db (use case)
+	// TODO init terminal
+}
